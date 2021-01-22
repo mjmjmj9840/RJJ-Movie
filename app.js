69139d606f982fccfca8b5f871e5905a3fb5ad35
Mijin <mjmjmj9840@gmail.com>
2021-01-22 14:16:53 +0900

    schedule crawling job once a day

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const { PythonShell } = require('python-shell');
+const schedule = require('node-schedule');
 require('dotenv').config();
 
 var app = express();
@@ -16,13 +17,15 @@ app.use(express.static('public'));
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Crawling Movies
-PythonShell.run('./python/naver_movie.py', null, function (err, data) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
+// Scheduling Crawling
+const j = schedule.scheduleJob('00 00 00 * * *', function () {
+  PythonShell.run('./python/naver_movie.py', null, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
 });
 
 // MongoDB Connect and Set
